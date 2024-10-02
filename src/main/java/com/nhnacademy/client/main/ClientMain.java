500682ccda51e613be5e97c8cfa29949d0d3dc57
JiwonLee-2 <this0aid@gmail.com>
2024-10-02 13:34:51 +0900

    EXERCISE01 socket

---
+++ src/main/java/com/nhnacademy/client/main/ClientMain.java
@@ -0,0 +1,28 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.client.main;
+
+import com.nhnacademy.client.runable.MessageClient;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+
+@Slf4j
+public class ClientMain {
+    public static void main(String[] args) {
+        //TODO#2-8 messageClient 객체를 생성하고 시작 합니다.
+        MessageClient messageClient = new MessageClient();
+        Thread thread = new Thread(messageClient);
+        thread.run();
+    }
+}
---
+++ src/main/java/com/nhnacademy/server/main/ServerMain.java
@@ -0,0 +1,29 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.server.main;
+
+import com.nhnacademy.server.runable.MessageServer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+
+@Slf4j
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+        //#1-10 MessageServer 객체를 생성하고 시작 합니다.
+        //왼쪽 녹색 화살표 버튼을 클릭 하면  서버를 실행 할 수 있습니다.
+        MessageServer messageServer = new MessageServer();
+        Thread thread = new Thread(messageServer);
+        thread.start();
+    }
+}
