0421f1a4e975777d883c50dd4b5e557c7b27de89
JiwonLee-2 <this0aid@gmail.com>
2024-10-08 17:19:46 +0900

    response-factory

--- src/main/java/com/nhnacademy/server/method/response/impl/LoginResponse.java
+++ src/main/java/com/nhnacademy/server/method/response/impl/LoginResponse.java
@@ -13,17 +13,32 @@
 package com.nhnacademy.server.method.response.impl;
 
 import com.nhnacademy.server.method.response.Response;
+import com.nhnacademy.server.runable.MessageServer;
+import com.nhnacademy.server.thread.channel.Session;
 
-public class EchoResponse implements Response {
-    private final static String METHOD = "echo";
+import java.util.List;
 
+public class LoginResponse implements Response {
     @Override
     public String getMethod() {
-        return METHOD;
+        return "login";
     }
 
     @Override
     public String execute(String value) {
-        return value;
+
+        if(value.equals("list")){
+            List<String> ids = MessageServer.getClientIds();
+            return ids.size() >0 ? String.join(System.lineSeparator(), ids) : "empty";
+        }
+
+        boolean loginSuccess = MessageServer.addClient(value, Session.getCurrentSocket());
+
+        if(loginSuccess){
+            Session.initializeId(value);
+            return  "login success!";
+        }
+
+        return "login fail";
     }
 }
